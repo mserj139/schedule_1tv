ca1f56aab7cdec4bd4fbb658fa4a530417f38795
sergeygildi <sergeygildi@gmai.com>
2018-09-06 21:43:01 +0300

    *fixed bugs

--- src/main/java/com/website/_1tv/config/WebSecurityConfig.java
+++ src/main/java/com/website/_1tv/config/WebSecurityConfig.java
@@ -27,7 +27,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
               .permitAll()
             .and()
               .logout()
-              .permitAll();
+              .permitAll(); 
     }
 
   @Override
